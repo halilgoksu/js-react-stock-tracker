398422c82aeee449f94035578564a9687567e7b6
unknown <halilskywater@gmail.com>
2023-01-18 18:23:08 -0700

    input focus

---
+++ .history/src/components/AutoComplete_20230118152324.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect, useContext } from "react"
+import finnHub from "../apis/finnHub"
+import { WatchListContext } from "../context/watchListContext"
+import {FaSearch} from "react-icons/fa"
+
+
+export const AutoComplete = () => {
+  const [search, setSearch] = useState("")
+  const [results, setResults] = useState([])
+  const { addStock } = useContext(WatchListContext)
+
+  const renderDropdown = () => {
+    const dropDownClass = search ? "show" : null
+    return (
+      <ul className={`dropdown-menu ${dropDownClass}`}>
+        {results.map((result) => {
+          return (
+            <li onClick={() => {
+              addStock(result.description )
+            }} key={result.symbol} className="dropdown-item">{result.description} ({result.symbol})
+          
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  useEffect(() => {
+    let isMounted = true
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/search", {
+          params: {
+            q: search
+          }
+        })
+
+        if (isMounted) {
+          setResults(response.data.result)
+        }
+
+      } catch (err) {
+
+      }
+    }
+    if (search.length > 0) {
+      fetchData()
+    } else {
+      setResults([])
+    }
+    return () => (isMounted = false)
+  }, [search])
+
+  return <div className="flex-col justify-center text-center items-center m-2">
+    <div className="flex-col w-full text-center  items-center justify-center">
+      <div className="flex w-full items-center text-center justify-center gap-2">
+      <input  id="search" type="text"
+       className=" inline text-white text-sm bg-black  w-1/3 form-control " placeholder="Search" autoComplete="off"
+        value={search} onChange={(e) => setSearch(e.target.value)}>
+      </input>
+      <button onClick={()=>setSearch} className="btn btn-dark text-sm">Clear</button>
+      </div>
+      <div className="flex justify-center items-center text-center">
+      {renderDropdown()}
+      </div>   
+    </div>
+    <FaSearch className="inline text-xl m-3 text-white "/>
+    
+
+  </div>
+}
---
+++ .history/src/pages/StockOverviewPage_20230118181107.jsx
@@ -0,0 +1,17 @@
+import { AutoComplete } from "../components/AutoComplete";
+import { StockList } from "../components/StockList";
+import {AiOutlineStock}from "react-icons/ai"
+export const StockOverviewPage = () => {
+  return (
+    <div className="flex-col text-center items-center justify-center  w-screen h-screen p-2">
+
+      <div className="flex flex-col text-center items-center justify-center 
+      bg-black text-white">
+        <AiOutlineStock className="flex  border-2 text-7xl shadow-lg"/> 
+        <h1 className="font-bold">Stocks Tracker</h1>
+      </div>
+      <AutoComplete className="flex z-20 h-2 over" />
+      <StockList className="flex z-0"/>
+    </div>
+  );
+};
